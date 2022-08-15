ec558f484cb21c067fb55f4034b9ec5e3f47f77a
laura karina Borja <109982186+LaurakarinaB@users.noreply.github.com>
2022-08-14 23:55:42 -0500

    subir
    subir

--- Taller Estructuras de Control Selectivas-1/Taller_15.py
+++ Taller Estructuras de Control Selectivas-1/Taller_15.py
@@ -1,26 +1,37 @@
 edad_en_meses=float(input(""))
-nivel_de_hemoglobina=float(input(""))
-rango_menor=float()
+rango_menor=float(input(""))
+
 if(edad_en_meses<=1):
-  rango_menor <= 13
+  if(rango_menor<=13):
+    print("Positivo en anemia")
+  else:
+    print("Negativo en anemia")
+    
 elif(edad_en_meses>1 and edad_en_meses<=6):
-  rango_menor<=10
+  if(rango_menor<=10):
+    print("Positivo en anemia")
+  else:
+    print("Negativo en anemia")
     
 elif(edad_en_meses>6 and edad_en_meses<=12): 
-  rango_menor<=11
+  if(rango_menor<=11):
+    print("Positivo en anemia")
+  else:
+    print("Negativo en anemia")
 
 elif(edad_en_meses>12 and edad_en_meses<=60):
-  rango_menor <- 11.5
-    
-elif(edad_en_meses>30 and edad_en_meses<=120): 
-  rango_menor <- 12.6
+  if(rango_menor <= 11.5):
+    print("Positivo en anemia")
+  else:
+    print("Negativo en anemia") 
+elif(edad_en_meses>30 and edad_en_meses<=120):
+  if(rango_menor<= 12.6):
+     print("Positivo en anemia")
+  else:
+    print("Negativo en anemia")
     
 elif(edad_en_meses>120 and edad_en_meses<=180): 
-  rango_menor <- 13
- 
-elif(nivel_de_hemoglobina < rango_menor):
-  print("Positivo en anemia")
-else:
-  print("Negativo en anemia")
-    
-print("Valor de rango menor: ", rango_menor)
+  if(rango_menor <= 13):
+     print("Positivo en anemia")
+  else:
+    print("Negativo en anemia")
